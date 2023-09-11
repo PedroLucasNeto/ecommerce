04ca8b463e8a6aed42fc424a7c537741fb209da4
Pedro Lucas <101678622+PedroLucasNeto@users.noreply.github.com>
2023-09-11 18:38:54 -0300

    feat: .

--- src/components/navbar/NavBarLink.tsx
+++ src/components/navbar/NavBarLink.tsx
@@ -7,7 +7,7 @@ icon?: string,
 }
 const NavBarLink = ({link, text, icon}:NavBarLinkProps) => {
   return (
-    <Link to={link} className="text-white p-2 rounded-md hover:border-b hover:border-purple-600 hover:text-purple-300 transition delay-100 duration-600 ease-in-out" target="blank">{text} {icon!=""? <i className={`text-white h-120 ${icon} text-lg `}></i> : 'T'}</Link>
+    <Link to={link} className="text-white p-2 rounded-md hover:border-b hover:border-purple-600 hover:text-purple-300 transition delay-100 duration-600 ease-in-out" >{text} {icon!=""? <i className={`text-white h-120 ${icon} text-lg `}></i> : 'T'}</Link>
   )
 }
 
